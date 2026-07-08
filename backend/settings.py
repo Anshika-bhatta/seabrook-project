6a18f5b9f2aa585508578857693cfce2146af5f3
unknown <anshikabhatt83@gmail.com>
2026-07-08 17:13:00 +0545

    Fix CORS_ALLOWED_ORIGINS blank string issue

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
-
 # Application definition
 
 INSTALLED_APPS = [
